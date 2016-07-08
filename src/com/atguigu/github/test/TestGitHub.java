5c8aba69d1c3215092b786168a3d91e8ad286c58
xiaoxuLove <742137498@qq.com>
2016-07-08 22:26:30 +0800

    hahaha
    haahha

--- src/com/atguigu/github/test/TestGitHub.java
+++ src/com/atguigu/github/test/TestGitHub.java
@@ -3,8 +3,9 @@ package com.atguigu.github.test;
 public class TestGitHub {
 	
 	public static void main(String[] args) {
-		System.out.println("hello github 服务器01");
-		System.out.println("hello github 服务器02");
+		System.out.println("hello github 路镁脦帽脝梅01");
+		System.out.println("hello github 路镁脦帽脝梅02");
+		System.out.println("nimeide luanma");
 	}
 
 }
