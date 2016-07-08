ede09bed02fd54db4aae16e7dcf9d85022934055
yangxu <742137498@qq.com>
2016-07-08 22:45:13 +0800

    update 003

--- src/com/atguigu/github/test/TestGitHub.java
+++ src/com/atguigu/github/test/TestGitHub.java
@@ -3,9 +3,11 @@ package com.atguigu.github.test;
 public class TestGitHub {
 	
 	public static void main(String[] args) {
-		System.out.println("hello github ·þÎñÆ÷01");
-		System.out.println("hello github ·þÎñÆ÷02");
+		System.out.println("hello github 服务器01");
+		System.out.println("hello github 服务器02");
 		System.out.println("nimeide luanma");
+		
+		System.out.println("hello github 服务器03");
 	}
 
 }
